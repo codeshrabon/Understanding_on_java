c00f12069b92781fce735a40f4c8defd2dbab530
Faysal Ahmed <19101043@uap-bd.edu>
2025-06-26 22:48:23 +0600

    Revisit in java

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("Here i am again in JAVA");
+        Person p1 = new Person();
+        p1.name = "faysal ahmed";
+        // using instance method here in main method we are printing it 
+        p1.sayHello();
+
+        System.out.println("Using Encapsulation class");
+        Encapsulation encap = new Encapsulation();
+
+
+        encap.setName("Sharbon");
+        System.out.println("From encapsulation --> my Name is " + encap.getName());
+        }
+}
+ 
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+
+    // instance method
+
+    void sayHello() {
+        System.out.println("Hello there, How are you");
+        System.out.println("Im from Person class and im an instance variable ");
+        System.out.println("My name is " + name);
+    }
+
+}
